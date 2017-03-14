2b1596e62fabafaa4b3966355272d5ef898df841
FlippinPizza <ph.osswald@gmail.com>
2017-03-14 21:47:07 +0100

    Implementation of a TreeMap

--- src/tree/Tree.java
+++ src/tree/Tree.java
@@ -1,5 +1,316 @@
 package tree;
 
-public class Tree {
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * This Class implements the Interface java.util.Map in a minimalistic manor
+ * 
+ * @author Flip42
+ *
+ * @param <K>
+ *            Key of Map
+ * @param <V>
+ *            stored Values
+ */
+public class Tree<K extends Comparable<K>, V> implements Map<K, V> {
+
+	private Tree<K, V> left;
+	private Tree<K, V> right;
+	private Entry<K, V> entry;
+	private int organizeCounter;
+
+	public Tree(K key, V value) {
+		this.entry = new Entry<K, V>(key, value);
+		this.left = null;
+		this.right = null;
+		this.organizeCounter = 0;
+	}
+
+	public Tree() {
+		this.entry = null;
+		this.left = null;
+		this.right = null;
+		this.organizeCounter = 0;
+	}
+
+	@Override
+	public int size() {
+		int retval = (entry != null) ? 1 : 0;
+		if (left != null) {
+			retval += left.size();
+		}
+		if (right != null) {
+			retval += right.size();
+		}
+		return retval;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return size() == 0;
+	}
+
+	@Override
+	public boolean containsKey(Object key) {
+		return get(key) != null;
+	}
+
+	@Override
+	public boolean containsValue(Object value) {
+		if (entry != null && entry.value.equals(value)) {
+			return true;
+		}
+		boolean retval = false;
+		if (left != null) {
+			retval = left.containsValue(value);
+		}
+		if (right != null && retval == false) {
+			retval = right.containsValue(value);
+		}
+		return retval;
+
+	}
+
+	@Override
+	public V get(Object key) {
+		if (getElement(key) != null) {
+			return getElement(key).entry.getValue();
+		}
+		return null;
+	}
+
+	private Tree<K, V> getElement(Object key) {
+		if (entry == null) {
+			return null;
+		}
+		if (entry.key.equals(key)) {
+			return this;
+		} else {
+			if (entry.compareTo((K) key) >= 0) {
+				if (left != null) {
+					return left.getElement(key);
+				}
+				return null;
+			} else {
+				if (right != null) {
+					return right.getElement(key);
+				}
+				return null;
+			}
+		}
+	}
+
+	@Override
+	public V put(K key, V value) {
+		if (entry == null) {
+			entry = new Entry<K, V>((K) key, (V) value);
+			return value;
+		}
+		if (entry.compareTo(key) >= 0) {
+			if (left == null) {
+				left = new Tree<K, V>();
+			}
+			left.put(key, value);
+		} else {
+			if (right == null) {
+				right = new Tree<K, V>();
+			}
+			right.put(key, value);
+		}
+		organize();
+		return (V) value;
+	}
+
+	@Override
+	public V remove(Object key) {
+		V value = null;
+		Tree<K, V> element = getElement(key);
+		if (element != null) {
+			value = element.entry.getValue();
+			element.entry = null;
+			Tree<K, V> tmpLeft = element.left;
+			Tree<K, V> tmpRight = element.right;
+			element.left = element.right = null;
+			element.putAll(tmpRight);
+			element.putAll(tmpLeft);
+		}
+		return value;
+	}
+
+	@Override
+	public void putAll(Map<? extends K, ? extends V> map) {
+		if (map != null) {
+			for (Map.Entry<? extends K, ? extends V> e : map.entrySet()) {
+				this.put(e.getKey(), e.getValue());
+			}
+		}
+	}
+
+	@Override
+	public void clear() {
+		left = null;
+		right = null;
+		entry = null;
+		organizeCounter = 0;
+	}
+
+	/**
+	 * balances the Tree at every 10th Change
+	 */
+	private void organize() {
+		organizeCounter++;
+		if (organizeCounter > 10) {
+			if (left != null) {
+				left = left.balance();
+			}
+			if (right != null) {
+				right = right.balance();
+			}
+		}
+	}
+
+	// see for further Info http://me-lrt.de/08-avl-baum-java-selbstbalanciert
+	public Tree<K, V> balance() {
+		Tree<K, V> root = this;
+		int leftSize = 0;
+		int rightSize = 0;
+		if (left != null) {
+			left = left.balance();
+			leftSize = left.size();
+		}
+		if (right != null) {
+			right = right.balance();
+			rightSize = right.size();
+		}
+		if (leftSize + 1 < rightSize) {
+			// Gegen den Uhrzeigersinn drehen
+			if (right != null) {
+				Tree<K, V> temp = right.left;
+				right.left = this;
+				root = this.right;
+				this.right = temp;
+				return root;
+			}
+		} else if (leftSize > rightSize + 1) {
+			// Im Uhrzeigersinn drehen
+			if (left != null) {
+				Tree<K, V> temp = left.right;
+				left.right = this;
+				root = this.left;
+				this.left = temp;
+				return root;
+			}
+		}
+		organizeCounter = 0;
+		return root;
+	}
+
+	@Override
+	public Set<K> keySet() {
+		Set<K> set = new HashSet<K>();
+		set.add(entry.key);
+		if (left != null) {
+			set.addAll(left.keySet());
+		}
+		if (right != null) {
+			set.addAll(right.keySet());
+		}
+		return set;
+	}
+
+	@Override
+	public Collection<V> values() {
+		Set<V> set = new HashSet<V>();
+		set.add(entry.value);
+		if (left != null) {
+			set.addAll(left.values());
+		}
+		if (right != null) {
+			set.addAll(right.values());
+		}
+		return set;
+	}
+
+	@Override
+	public Set<Map.Entry<K, V>> entrySet() {
+		Set<Map.Entry<K, V>> set = new HashSet<Map.Entry<K, V>>();
+		set.add(entry);
+		if (left != null) {
+			set.addAll(left.entrySet());
+		}
+		if (right != null) {
+			set.addAll(right.entrySet());
+		}
+		return set;
+	}
+
+	public class Entry<K extends Comparable<K>, V> implements Map.Entry<K, V>, Comparable<K> {
+
+		private K key;
+		private V value;
+
+		public Entry(K key, V value) {
+			this.key = key;
+			this.value = value;
+		}
+
+		@Override
+		public K getKey() {
+			return key;
+		}
+
+		@Override
+		public V getValue() {
+			return value;
+		}
+
+		@Override
+		public V setValue(V value) {
+			V oldValue = this.value;
+			this.value = value;
+			return oldValue;
+		}
+
+		@Override
+		public int compareTo(K o) {
+			return this.key.compareTo(o);
+		}
+
+		public int compareTo(Entry<K, V> o) {
+			return this.key.compareTo(o.getKey());
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Entry<K, V> other = (Entry) obj;
+			if (!getOuterType().equals(other.getOuterType()))
+				return false;
+			if (key == null) {
+				if (other.key != null)
+					return false;
+			} else if (!key.equals(other.key))
+				return false;
+			if (value == null) {
+				if (other.value != null)
+					return false;
+			} else if (!value.equals(other.value))
+				return false;
+			return true;
+		}
+
+		private Tree getOuterType() {
+			return Tree.this;
+		}
+
+	}
 
 }
--- test/tree/TreeTest.java
+++ test/tree/TreeTest.java
@@ -1,5 +1,186 @@
 package tree;
 
-public class TreeTest {
 
+import static org.junit.Assert.*;
+import org.junit.*;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+
+public class TreeTest {
+	
+	private Map<Integer, String> tree;
+
+	@Before
+	public void init(){
+		tree = new Tree<Integer,String>();
+	}
+
+	@Test
+	public void enshureJUnitWorks() {
+		assertTrue("There is something wrong with JUnit.",true);
+	}
+
+	@Test
+	public void sizeOfEmptyMap() {
+		assertEquals("Map is not Empty",0,tree.size());
+	}
+
+	@Test
+	public void sizeOfOneEntryMap() {
+		tree.put(42, "The Meaning of Life");
+		assertEquals("Map is Empty",1,tree.size());
+	}
+
+	@Test
+	public void sizeOfTwoEntryMap() {
+		tree.put(42, "The Meaning of Life");
+		tree.put(1, "I'm the no One!");
+		assertEquals("Map is Empty",2,tree.size());
+	}
+
+	@Test
+	public void sizeOfThreeEntryMap() {
+		tree.put(42, "The Meaning of Life");
+		tree.put(1, "I'm the no One!");
+		tree.put(100, "Bestof");
+		assertEquals("Map is Empty",3,tree.size());
+	}
+
+	@Test
+	public void getOneFromMap() {
+		tree.put(42, "The Meaning of Life");
+		assertEquals("Map doesn' contain an Element at Index 42","The Meaning of Life",tree.get(42));
+	}
+
+	@Test
+	public void getTwoFromMap() {
+		tree.put(42, "The Meaning of Life");
+		tree.put(1, "I'm the no One!");
+		assertEquals("Map doesn' contain an Element at Index 42","The Meaning of Life",tree.get(42));
+		assertEquals("Map doesn' contain an Element at Index 1","I'm the no One!",tree.get(1));
+	}
+
+	@Test
+	public void getThreeFromMap() {
+		tree.put(42, "The Meaning of Life");
+		tree.put(1, "I'm the no One!");
+		tree.put(100, "Bestof");
+		assertEquals("Map doesn' contain an Element at Index 42","The Meaning of Life",tree.get(42));
+		assertEquals("Map doesn' contain an Element at Index 1","I'm the no One!",tree.get(1));
+		assertEquals("Map doesn' contain an Element at Index 100","Bestof",tree.get(100));
+	}
+
+	@Test
+	public void isEmptyMap() {
+		assertTrue("Map is not Empty",tree.isEmpty());
+	}
+
+	@Test
+	public void isNotEmptyMap() {
+		tree.put(42, "The Meaning of Life");
+		assertFalse("Map is Empty",tree.isEmpty());
+	}
+
+	@Test
+	public void mapContainsKey() {
+		tree.put(42, "The Meaning of Life");
+		assertTrue("Map does not contain Key 42",tree.containsKey(42));
+	}
+
+	@Test
+	public void mapDoesntContainKey() {
+		assertFalse("Map does contain Key 42",tree.containsKey(42));
+	}
+
+	@Test
+	public void mapContainsValue() {
+		tree.put(42, "42");
+		assertTrue("Map does not contain Value 42",tree.containsValue("42"));
+	}
+
+	@Test
+	public void mapContainsValueOutOfTwo() {
+		tree.put(1, "1");
+		tree.put(42, "42");
+		assertTrue("Map does not contain Value 42",tree.containsValue("42"));
+	}
+
+	@Test
+	public void mapContainsValueOutOfThree() {
+		tree.put(1, "1");
+		tree.put(100, "100");
+		tree.put(42, "42");
+		assertTrue("Map does not contain Value 42",tree.containsValue("42"));
+	}
+
+	@Test
+	public void removeElementfromTree() {
+		tree.put(1, "1");
+		tree.put(100, "100");
+		tree.put(42, "42");
+		tree.remove(1);
+		assertFalse("Map does contain Key 1",tree.containsKey(1));
+	}
+
+	@Test
+	public void mapDoesntContainValue() {
+		assertFalse("Map does contain Value 42",tree.containsValue("42"));
+	}
+
+	@Test
+	public void putAll() {
+		Map<Integer, String> init = new HashMap<Integer,String>();
+		for(int i=0;i<10;i++){
+			init.put(i, String.valueOf(i));
+		}
+		tree.putAll(init);
+		assertEquals("putAll() failed",10,tree.size());
+	}
+
+	@Test
+	public void clearThree() {
+		tree.put(1, "1");
+		tree.put(42, "42");
+		tree.clear();
+		assertEquals("Map is not cleared",0,tree.size());
+	}
+
+	@Test
+	public void getKeySet() {
+		tree.put(1, "1");
+		tree.put(2, "2");
+		tree.put(3, "3");
+		tree.put(42, "42");
+		Set<Integer> set = tree.keySet();
+		assertTrue("keySet() misses a Key",set.contains(1));
+		assertTrue("keySet() misses a Key",set.contains(2));
+		assertTrue("keySet() misses a Key",set.contains(3));
+		assertTrue("keySet() misses a Key",set.contains(42));
+	}
+
+	@Test
+	public void MapValues() {
+		tree.put(1, "1");
+		tree.put(2, "2");
+		tree.put(3, "3");
+		tree.put(42, "42");
+		Collection<String> set = tree.values();
+		assertTrue("values() misses a Value",set.contains("1"));
+		assertTrue("values() misses a Value",set.contains("2"));
+		assertTrue("values() misses a Value",set.contains("3"));
+		assertTrue("values() misses a Value",set.contains("42"));
+	}
+
+
+	@Test
+	public void performaceTest() {
+		for(int i=0;i<10000;i++){
+			tree.put(i, String.valueOf(i));
+		}
+		assertEquals("performaceTest() failed",10000,tree.size());
+	}
 }
